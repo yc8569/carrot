2dfc2dffc0b6ca2bdcdced13f6e2285aab68b8f6
doidoe <limoh19@naver.com>
2022-08-20 15:43:31 +0900

    커밋

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import user from "./modules/user";
+
+
+
 import post from "./modules/post";
 
 const store = configureStore({
---
+++ src/shared/localStorage.js
@@ -0,0 +1,11 @@
+export function saveToken(token) {
+    localStorage.setItem("token", token);
+}
+
+export function getToken() {
+    return localStorage.getItem("token");
+}
+
+export function removeToken() {
+    localStorage.removeItem("token");
+}
